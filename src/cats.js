751709f5c58783d8469352b4fe291cc9d0525e41
CyrkaFRDE <159411247+CyrkaFRDE@users.noreply.github.com>
2024-03-29 11:33:05 +0100

    Update display dynamic

--- src/cats.js
+++ src/cats.js
@@ -13,7 +13,14 @@
  * @return {boolean} True if the selector with name name has no children (is disabled). False otherwise.
  */
 function isDisabled(name) {
-    return document.querySelector('#' + name).children.length == 0;
+    var element = document.querySelector('#' + name);
+    // Check if the element exists before trying to access its children
+    if (element) {
+        return element.children.length == 0;
+    } else {
+        // If the element doesn't exist, you could consider it as disabled or handle as needed
+        return true; // Assumes that if the element does not exist, it is considered disabled
+    }
 }
 
 /**
@@ -176,27 +183,6 @@ var selector_values_after_sp_1 = {
     "ji": {
         "acc": {
             "v1": {
-                "title": "select_sg",
-                "values": [
-                    {
-                        "value": "tp",
-                        "desc": "desc"
-                    },
-                    {
-                        "value": "o65",
-                        "desc": "desc"
-                    },
-                    {
-                        "value": "u18",
-                        "desc": "desc"
-                    },
-                    {
-                        "value": "ng",
-                        "desc": "desc"
-                    }
-                ]
-            },
-            "amenity": {
                 "title": "select_amenity",
                 "values": [
                     {
@@ -303,7 +289,7 @@ var selector_values_after_sp_1 = {
     },
     "ji_v_sg": {
         "acc": {
-            "v1": {
+            "amenity": {
                 "title": "select_sg",
                 "values": [
                     {
@@ -324,7 +310,7 @@ var selector_values_after_sp_1 = {
                     }
                 ]
             },
-            "amenity": {
+            "v1": {
                 "title": "select_amenity",
                 "values": [
                     {
@@ -368,7 +354,7 @@ var selector_values_after_sp_1 = {
             }
         },
         "exp": {
-            "v1": {
+            "amenity": {
                 "title": "select_sg",
                 "values": [
                     {
@@ -397,7 +383,7 @@ var selector_values_after_sp_1 = {
                     }
                 ]
             },
-            "amenity": {
+            "v1": {
                 "title": "select_exp",
                 "values": [
                     {
@@ -416,7 +402,7 @@ var selector_values_after_sp_1 = {
             }
         },
         "ava": {
-            "v1": {
+            "amenity": {
                 "title": "select_sg",
                 "values": [
                     {
@@ -445,7 +431,7 @@ var selector_values_after_sp_1 = {
                     }
                 ]
             },
-            "amenity": {
+            "v1": {
                 "title": "select_ava",
                 "values": [
                     {
@@ -464,7 +450,7 @@ var selector_values_after_sp_1 = {
             }
         },
         "beh": {
-            "v1": {
+            "amenity": {
                 "title": "select_sg",
                 "values": [
                     {
@@ -493,7 +479,7 @@ var selector_values_after_sp_1 = {
                     }
                 ]
             },
-            "amenity": {
+            "v1": {
                 "title": "select_beh",
                 "values": [
                     {
@@ -675,8 +661,8 @@ function updateSelectorAfterSP0(name, map_type_value) {
         // Add function when checked
         $('input[type=radio][name=' + name + ']').change(function() {
             updateSelectorAfterSP1("v1", this.value);
-            updateSelectorAfterSP1("amenity", this.value);
-            updateSelectorAfterSP1("mot", this.value);
+            updateSelectorAfterSP2("amenity", this.value);
+            updateSelectorAfterSP2("mot", this.value);
             
             if (correctValues()) {
                 changeMap();
@@ -695,15 +681,19 @@ function updateSelectorAfterSP0(name, map_type_value) {
 function updateSelectorAfterSP1(name, justice_value) {
     let map_type_value = getValue("map_type");
 
-    let sp = {
-        "v1": "sp-2",
-        "amenity": "sp-3",
-        "mot": "sp-4"
+   let existentSP2 = document.querySelector('#sp-2');
+    if (existentSP2) {
+        existentSP2.remove();
     }
 
-    // Find the .select-main to change
-    let curr_sp = document.querySelector('#' + sp[name]);
-    curr_sp.style.display = 'block';
+    let curr_sp = document.createElement('div');
+    curr_sp.setAttribute('class', 'select-main');
+    curr_sp.setAttribute('id', 'sp-2');
+    curr_sp.style.display = 'none'; // Initially hidden, show it when ready
+
+    let checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+    curr_sp.appendChild(checkbox);
 
     try {
         // Change its title
@@ -749,6 +739,12 @@ function updateSelectorAfterSP1(name, justice_value) {
             }
             select_options.appendChild(option);
         }
+        curr_sp.style.display = 'block';
+        curr_sp.style.marginLeft = '30px'; // cyrine
+
+        let selectedOption = document.querySelector('#sp-1 input[type="radio"]:checked').parentNode;
+        selectedOption.insertAdjacentElement('afterend', curr_sp);
+
 
         // Unfold in the interface
         curr_sp.querySelector("input[type=checkbox]").checked = true;
@@ -768,13 +764,110 @@ function updateSelectorAfterSP1(name, justice_value) {
     }
     translatePage();
 }
+function updateSelectorAfterSP2(name, justice_value)  {
+    let map_type_value = getValue("map_type");
+    let sp = {
+        "amenity": "sp-3",
+        "mot": "sp-4"
+    }
+    // Find the .select-main to change
+    let existentSP = document.querySelector('#' + sp[name]);
+    if (existentSP) {
+        existentSP.remove();
+    }
+    let curr_sp = document.createElement('div');
+    curr_sp.setAttribute('class', 'select-main');
+    curr_sp.setAttribute('id',  sp[name]);
+    curr_sp.style.display = 'none'; // Initially hidden, show it when ready
+    curr_sp.style.borderTop = '1px solid var(--tum-blue-dark)';
 
+    let titleDiv = document.createElement('div');
+    titleDiv.className = 'select-title-text';
+    titleDiv.setAttribute('i18n', 'select'); // Setting the i18n attribute for internationalization
+    curr_sp.appendChild(titleDiv);
+
+    let selectOptionsDiv = document.createElement('div');
+    selectOptionsDiv.className = 'select-options';
+    selectOptionsDiv.setAttribute('id', name); // Consider dynamic IDs or classes if multiple instances
+    curr_sp.appendChild(selectOptionsDiv);
+
+    try {
+
+        let select_title_text = curr_sp.querySelector('.select-title-text');
+        select_title_text.setAttribute("i18n", selector_values_after_sp_1[map_type_value][justice_value][name]["title"])
+
+        // Fetch possible values
+        let selector_dict = selector_values_after_sp_1[map_type_value][justice_value][name]["values"];
+        if (selector_dict.length == 0) { throw EvalError; }
+
+        // Empty select options
+        let select_options = curr_sp.querySelector(".select-options");
+        select_options.innerHTML = "";
+
+        // Create the different options
+        for (const v of selector_dict) {
+            var option = document.createElement("div");
+            option.setAttribute("class", "select-option");
+
+            var radio = document.createElement("input");
+            radio.setAttribute("type", "radio");
+            radio.setAttribute("name", name);
+            radio.setAttribute("id", v["value"]);
+            radio.setAttribute("value", v["value"]);
+            option.appendChild(radio);
+
+            var label = document.createElement("label");
+            label.setAttribute("for", v["value"]);
+            label.setAttribute("i18n", v["value"]);
+            option.appendChild(label);
+
+            if (v["desc"] !== "desc") {
+                var info = document.createElement("div");
+                info.setAttribute("class", "select-title-info");
+                info.innerHTML = "&#x1F6C8;";
+
+                var info_tooltip = document.createElement("span");
+                info_tooltip.setAttribute("class", "info-tooltip");
+                info_tooltip.setAttribute("i18n", v["desc"]);
+                info.appendChild(info_tooltip);
+
+                option.appendChild(info);
+            }
+            select_options.appendChild(option);
+        }
+        curr_sp.style.display = 'block';
+        curr_sp.style.marginLeft = '30px';
+        let selectedOption = document.querySelector('#sp-1 input[type="radio"]:checked').parentNode;
+        let nextSibling = selectedOption.nextElementSibling;
+        if (nextSibling) {
+            nextSibling.insertAdjacentElement('afterend', curr_sp);
+        } else {
+            // If there is no next sibling, just insert curr_sp after the selectedOption
+            selectedOption.insertAdjacentElement('afterend', curr_sp);
+        }
+        //curr_sp.querySelector("input[type=checkbox]").checked = true;
+
+        // Add function when checked
+        $('input[type=radio][name=' + name + ']').change(function() {
+            if (correctValues()) {
+                changeMap();
+            }
+        });
+    } catch (error) {
+        curr_sp.style.display = 'none';
+
+        // Empty select options
+        let select_options = curr_sp.querySelector(".select-options");
+        select_options.innerHTML = "";
+    }
+    translatePage();
+}
 // Add callback for SP0
 $('input[type=radio][name=map_type]').change(function() {
     updateSelectorAfterSP0("justice", this.value);
     updateSelectorAfterSP1("v1", "", "");
-    updateSelectorAfterSP1("amenity", "", "");
-    updateSelectorAfterSP1("mot", "", "");
+    updateSelectorAfterSP2("amenity", "", "");
+    updateSelectorAfterSP2("mot", "", "");
 
     if (correctValues()) {
         changeMap();
